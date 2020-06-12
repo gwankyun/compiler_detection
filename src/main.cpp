4d38dbf88d3d555742af81eaab4035f0eb7915a2
權少 <ljqican@163.com>
2020-06-12 21:59:30 +0800

    添加簡單的測試項目

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <cstddef> // NULL
+#include "compile_features.h"
+
+int main()
+{
+#ifdef CXX_NULLPTR
+    int* n = nullptr;
+#else
+    int* n = NULL;
+#endif // CXX_NULLPTR
+
+    return 0;
+}
